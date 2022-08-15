562e63b96c47e01623f699cea6f986185681e0ab
ingodinho <siemens.ingo@googlemail.com>
2022-08-15 13:05:41 +0200

    React Router

---
+++ frontend/src/GlobalStyles.js
@@ -0,0 +1,69 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+html, body {
+  height: 100%;
+}
+
+body {
+  max-width: 500px;
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+}
+
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+
+input, button, textarea, select {
+  font: inherit;
+}
+
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+
+#root, #__next {
+  isolation: isolate;
+}
+
+/* ===================================
+  Fonts, Variables
+*/ ===================================
+
+//FIX ME --- FUCKING FONT GEHT NICHT
+
+//@font-face {
+//  font-family: 'SF_Pro';
+//  src: url('./fonts/SF-Pro-Text-Regular.ttf') format("truetype");
+//}
+//
+//body {
+//  font-family: 'SF_Pro', serif;
+//}
+
+:root {
+  --clr-blue: #1D9BF0;
+  --clr-text-prm: #141619;
+  --clr-text-white: #fff;
+  --clr-text-grey: #687684;
+  --clr-text-link: #4C9EEB;
+  --clr-bg-grey: #E7ECF0;
+}
+
+`
+
+export default GlobalStyles;
