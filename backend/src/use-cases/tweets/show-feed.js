b607a101a0ab3c0bde53fb3ad232287f57bdc455
psiegmund <patrick-siegmund@web.de>
2022-08-16 16:50:40 +0200

    tweetById

--- backend/src/use-cases/tweets/show-feed.js
+++ backend/src/use-cases/tweets/show-feed.js
@@ -1,7 +1,7 @@
 import TweetsDAO from "../../db-access/tweets-dao.js";
 
 export const findAll = async () => {
-    const allTweeds = await TweetsDAO.findAllTweets();
-    console.log(allTweeds);
-    return allTweeds;
+    const allTweets = await TweetsDAO.findAllTweets();
+    console.log(allTweets);
+    return allTweets;
 }
