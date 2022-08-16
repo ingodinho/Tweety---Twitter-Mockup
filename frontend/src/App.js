04752ffaa26c4a4746991e158305dcab796db890
devwulff <p.wulff887@gmail.com>
2022-08-16 10:16:27 +0200

    LandingPage & StartPage added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import GlobalStyles from "./GlobalStyles";
-import {Routes, Route, useLocation} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Home from "./components/pages/Home/Home";
 import BottomNav from "./components/shared/BottomNav";
@@ -7,24 +7,29 @@ import LoginPage from "./components/pages/Login/LoginPage";
 import RegisterPage from "./components/pages/Register/RegisterPage";
 import TweetDetails from "./components/pages/TweetDetails/TweetDetails";
 import Profile from "./components/pages/ProfilePage/Profile";
+import StartPage from "./components/pages/Start/StartPage";
+import LandingPage from "./components/pages/Landing/LandingPage";
 
 function App() {
+  const location = useLocation();
 
-    const location = useLocation();
-
-    return (
-        <div className="App">
-            <Routes>
-                <Route path={"/home"} element={<Home/>}/>
-                <Route path={'/tweet/:id'} element={<TweetDetails/>}/>
-                <Route path={'/profile/:id'} element={<Profile/>}/>
-                <Route path={"/login"} element={<LoginPage/>}/>
-                <Route path={"/register"} element={<RegisterPage/>}/>
-            </Routes>
-            {location.pathname === '/register' || location.pathname === '/login' || <BottomNav/>}
-            <GlobalStyles/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path={"/"} element={<LandingPage />} />
+        <Route path={"/home"} element={<Home />} />
+        <Route path={"/tweet/:id"} element={<TweetDetails />} />
+        <Route path={"/profile/:id"} element={<Profile />} />
+        <Route path={"/login"} element={<LoginPage />} />
+        <Route path={"/register"} element={<RegisterPage />} />
+        <Route path={"/start"} element={<StartPage />} />
+      </Routes>
+      {location.pathname === "/register" || location.pathname === "/login" || (
+        <BottomNav />
+      )}
+      <GlobalStyles />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/styles/Buttons.js
+++ frontend/src/styles/Buttons.js
@@ -6,8 +6,22 @@ export const ButtonBig = styled.button`
   background-color: var(--clr-blue);
   width: 80vw;
   padding: 0.5rem 6rem;
-  font-size: 1.6rem;
+  font-size: 1.7rem;
+  font-weight: 700;
   color: var(--clr-text-white);
   border-radius: 30px;
   border: 0;
+`;
+
+export const ButtonLogin = styled.button`
+  display: block;
+  margin: 2rem auto;
+  background-color: var(--clr-text-white);
+  width: 80vw;
+  padding: 0.5rem 6rem;
+  font-size: 1.7rem;
+  font-weight: 700;
+  color: var(--clr-text-prm);
+  border-radius: 30px;
+  border: 1px solid var(--clr-line-grey);
 `;
